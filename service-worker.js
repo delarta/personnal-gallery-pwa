001de5c62b2bbf9696fca59704b14658ca217120
Delarta <mac@delarta-mac.local>
2019-11-01 23:55:51 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "personalgallery-v1"
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/main.js",
+  "/icon.png",
+  "/img/avatar.png",
+  "./img/underwater.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function(response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+ 
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
